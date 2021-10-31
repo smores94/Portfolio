04009dd57e3452ec9edf6b21c17ccfbd31b6b999
smores94 <89810630+smores94@users.noreply.github.com>
2021-10-31 17:08:30 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+sketch.js
+
+function setup() {
+  createCanvas(500, 600);
+}
+
+function setup() {
+    createCanvas(500, 600);
+  }
+  
+  function draw() {
+    background(255, 235, 205);
+    noFill()
+    ellipse(250,230,250,360);
+   
+    
+    noFill();
+    ellipse(190,140,50,30);
+    ellipse(300,140,50,30);
+    fill('rgba(0,255,0, 0.25)'); 
+    circle(300,140,30);
+    circle(190,140,30);
+    point(300, 140);
+    point(190, 140);
+  
+  
+    
+    fill('rgba(255, 250,240,0.5)');
+    triangle(230, 235, 250, 160, 275, 235);
+  
+    fill('rgba(255, 153, 153, 1.0)');
+    ellipse(250,300,200,50);
+    ellipse(250,300,175,30);
+    
+     
+  
+     
+    noFill();
+    strokeWeight(5); // Beastly
+    fill('rgba(230, 204, 179,0.9)');
+    rect(150,40,200,60);
+    line(250, 560, 250, 410);
+    textAlign(LEFT);
+    textSize(16);
+    strokeWeight(0.9);
+    text('Rose Krieg', 50, 70);
+  
+  
+      
+    
+  }
