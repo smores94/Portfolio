710b7258744437776867de9f2920067a492bd8c0
smores94 <89810630+smores94@users.noreply.github.com>
2021-11-26 18:30:18 -0700

    go

--- gamepage2.js
+++ gamepage2.js
@@ -1,25 +1,73 @@
-myButton = new Clickable();
+var x = 50;
+var y = 50;
+var diameter = 25;
+var y;
+var changeDirection;
+var x;
+var changeDirection;
+var speedX = 2.5;
+var speedY= 1.6;
+var position
+var myGamePiece;
+var myUpBtn;
+var myDownBtn;
+var myLeftBtn;
+var myRightBtn;
+var myGamePiece;
+var myGameArea;
+var objImage = null;
+var key_code = e.which || e.keyCode
 
+function setup() {
 
-// this function is called only once
-function setup()
-{
-
-    createCanvas(600,600);
-    background(249, 178, 196);
-
+  createCanvas(800, 600);
 }
 
-function draw()
-            {
-                
-            }
-myButton.draw();{ // <- Draw the 'myButton' Clickable
-            //...
-          }
-myButton = new Clickable();     //Create button
-myButton.locate(20, 20);        //Position Button
-myButton.onPress = function(){  //When myButton is pressed
-  this.color = "#AAAAFF";       //Change button color
-  alert("Yay!");                //Show an alert message
+<script type="text/javascript">
+    //init object globally
+    {
+           
+        function init() {
+        objImage = document.getElementById("myImage");
+        objImage.style.position = "relative";
+        objImage.style.left = "0px";
+        objImage.style.top = "0px";
+    }
+
+    function getKeyAndMove(e);{
+           switch (key_code);
+            case 37: //left arrow key
+                moveLeft();
+                break;
+            case 38: //Up arrow key
+                moveUp();
+                break;
+            case 39: //right arrow key
+                moveRight();
+                break;
+            case 40: //down arrow key
+                moveDown();
+                break;
+        
+    }
+    function moveLeft() {
+        objImage.style.left = parseInt(objImage.style.left) - 5 + "px";
+    }
+    function moveUp() {
+        objImage.style.top = parseInt(objImage.style.top) - 5 + "px";
+    }
+    function moveRight() {
+        objImage.style.left = parseInt(objImage.style.left) + 5 + "px";
+    }
+    function moveDown() {
+        objImage.style.top = parseInt(objImage.style.top) + 5 + "px";
+    }
+    window.onload = init;
+</script>
+
+
+function draw() {
+    background(155, 234, 251);
+    fill(247, 6, 35);
+    square(30,40,50);
 }
